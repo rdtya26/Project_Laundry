ca5aef075ae8f587881a6f9a72f13523272c03e5
ZahwaLeafathraa <zahwaleafathra@gmail.com>
2023-11-17 09:22:17 +0700

    push

---
+++ app/src/main/java/com/example/project_laundry/recycler_setrika.kt
@@ -0,0 +1,11 @@
+package com.example.project_laundry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recycler_setrika : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_setrika)
+    }
+}
